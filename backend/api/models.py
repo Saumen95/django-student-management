af5d928d3c57fda01b729525bcd1f326b27369e4
Saumen95 <saumenroy323@gmail.com>
2025-04-30 22:58:07 +0600

    implemented tagging using category

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,8 +7,18 @@ class Note(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="categories")
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 
 
