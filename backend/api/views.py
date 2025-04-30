b63f65814510cdcb72de46f1d9449574488f06ab
Saumen95 <saumenroy323@gmail.com>
2025-04-30 21:07:08 +0600

    backend codes complete

---
+++ backend/api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer, NoteSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Note
+
+
+class CreateNoteView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class DeleteNoteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+class CreateuserView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
